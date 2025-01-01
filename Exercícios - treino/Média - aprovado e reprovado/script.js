506d84d345b121855209f6f54b4f88ac55116cad
Trindade <juju.pradotrindade@gmail.com>
2025-01-01 15:10:03 -0300

    Finalizada a programação de média
    não fiz css ainda

--- Exercícios - treino/Média - aprovado e reprovado/script.js
+++ Exercícios - treino/Média - aprovado e reprovado/script.js
@@ -1,11 +1,22 @@
 function calcular(){
-    var mat = Number.parseFloat(document.getElementById('mat'))
-    var port = Number.parseFloat(document.getElementById('port'))
+    var tmat = document.getElementById('mat')
+    var tport = document.getElementById('port')
     var res = document.getElementById('res')
-    var media = (mat + port)/2
 
-     res.innerHTML = `Sua nota em Português foi ${port}`
-     res.innerHTML += `Sua nota em Matemática foi ${mat}`
-     res.innerHTML += `Sua média foi ${media}`
+    var mat = Number(tmat.value)
+    var por = Number(tport.value)
+
+    var media = (mat + por)/2
+
+     res.innerHTML = `Sua nota em Português foi ${por} <br>`
+     res.innerHTML += `Sua nota em Matemática foi ${mat} <br>`
+     res.innerHTML += `Sua média foi ${media} <br>`
      
+     if(media >= 9){
+        res.innerHTML += `Você foi <strong>APROVADO</strong> com distinção! <br>`
+     } else if(media >=7) {
+        res.innerHTML += `Você foi <strong>APROVADO</strong>! <br>`
+     } else{
+        res.innerHTML += `Você foi <strong>REPROVADO</strong>! <br>`
+     }
 }
