b98637c7c30b048f2bd78be93170050a017491a3
Trindade <juju.pradotrindade@gmail.com>
2025-01-01 22:56:05 -0300

    iniciado script reajuste salarial

---
+++ Exercícios - treino/Reajuste Salarial/script.js
@@ -0,0 +1,10 @@
+function verificar(){
+    var salariotxt = document.getElementById('salario')
+    var salario = Number(salariotxt.value)
+    var res = document.getElementById('res')
+    var aumento = 0
+
+    if (salario >= 280){
+        var aumento = 
+    }
+}
