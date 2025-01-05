31d5edeaa8bcb2a7a3dca6aa7a440ebc199bc44a
Trindade <juju.pradotrindade@gmail.com>
2025-01-05 12:06:51 -0300

    iniciado exercicio equação do segundo grau

---
+++ Exercícios - treino/equação/equacao.js
@@ -0,0 +1,12 @@
+function calcular(){
+    var a = Number(document.getElementById('a').value)
+    var b = Number(document.getElementById('b').value)
+    var c = Number(document.getElementById('c').value)
+    var res = document.getElementById('res')
+
+    var delta = b**-4*a*c
+    var raiz1 = (- b + Math.sqrt(delta) ) / 2 * a
+    var raiz2 = (- b - Math.sqrt(delta) ) / 2 * a
+
+    res.innerHTML = `Equação inserida: ${a}x² + ${b}x + ${c} = 0`
+}
